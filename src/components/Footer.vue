09fe12a251b95b97ca19bdbd079445a713cb47d1
eliiks <eliiks.rs@gmail.com>
2025-01-10 15:24:18 +0100

    Update contributions

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="bg-black text-white p-5 flex flex-col justify-center items-center gap-5 text-md md:text-lg text-center text-balance">
-        <p> Work in progress </p>
+        <p> Site réalisé par <a href="https://github.com/eliiks" class="underline text-blue-500"> Eliiks </a> </p>
         <p> Données météorologiques fournies par <a href="https://api.meteo-concept.com/" class="underline text-blue-500">Météo Concept</a></p>
         <p> Pictogrammes fournis par <a href="https://erikflowers.github.io/weather-icons/" class="underline text-blue-500"> Weather Icons </a> </p>
     </footer>
