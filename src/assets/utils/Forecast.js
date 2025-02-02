b6c7aa628c6f6e83a549160e1899b577d2ae28fc
eliiks <eliiks.rs@gmail.com>
2025-02-02 22:11:50 +0100

    Fixed date in english instead of french

--- src/assets/utils/Forecast.js
+++ src/assets/utils/Forecast.js
@@ -13,7 +13,7 @@ export default class Forecast{
         this.cityName = cityName
 
         let dateTime = new Date(forecastData.datetime);
-        this.date = dateTime.toLocaleString("default",  {day:"numeric", weekday: "long", month: "long", year:"numeric"});
+        this.date = dateTime.toLocaleString("fr-FR",  {day:"numeric", weekday: "long", month: "long", year:"numeric"});
         this.date = this.date.charAt(0).toUpperCase() + this.date.slice(1); //Upper the first letter
 
         this.weatherName = weatherCodeDict[forecastData.weather].name
